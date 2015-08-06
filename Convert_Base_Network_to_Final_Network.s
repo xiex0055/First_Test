e6d97760750c25d94661b1dffb86769690d9ab98
xiex0055 <xiex0055@gmail.com>
2015-08-06 13:48:09 -0400

    Debug the script
    bugs fixed

--- Convert_Base_Network_to_Final_Network.s
+++ Convert_Base_Network_to_Final_Network.s
@@ -6,17 +6,17 @@
 
 
 
-RUN PGM=HIGHTWAY
+RUN PGM=NETWORK
 
 FILEI NETI=basezonehwy.net	;input base network
 FILEO NETO=finalzonehwy.net	;output final network
 
 
-if(li.tollgrp>2)	;HOT lane links with tollgrp>2
-	if(li.amlimit=3 && li.pmlimit=3 && li.oplimit=3) amlimit=4 pmlimit=4 oplimit=4	;I-66 outside the Beltway; Beltway
-	if(li.amlimit=3 && li.pmlimit=3 && li.oplimit=9) amlimit=4 pmlimit=4 oplimit=9	;I-66 inside the Beltway
-	if(li.amlimit=3 && li.pmlimit=9 && li.oplimit=3) amlimit=4 pmlimit=9 oplimit=4	;I-95 NB
-	if(li.amlimit=9 && li.pmlimit=3 && li.oplimit=3) amlimit=9 pmlimit=4 oplimit=4	;I-95 SB
+if(li.1.tollgrp>2)	;HOT lane links with tollgrp>2
+	if(li.1.amlimit=3 && li.1.pmlimit=3 && li.1.oplimit=3) amlimit=4 pmlimit=4 oplimit=4	;I-66 outside the Beltway; Beltway
+	if(li.1.amlimit=3 && li.1.pmlimit=3 && li.1.oplimit=9) amlimit=4 pmlimit=4 oplimit=9	;I-66 inside the Beltway
+	if(li.1.amlimit=3 && li.1.pmlimit=9 && li.1.oplimit=3) amlimit=4 pmlimit=9 oplimit=4	;I-95 NB
+	if(li.1.amlimit=9 && li.1.pmlimit=3 && li.1.oplimit=3) amlimit=9 pmlimit=4 oplimit=4	;I-95 SB
 endif
 
 ENDRUN
